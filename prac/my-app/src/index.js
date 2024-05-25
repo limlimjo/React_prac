93254ddf48ac3ff0cca65f087820a44714cb8416
selim-jo <skyblue.777@daum.net>
2024-05-25 16:58:13 +0900

    240525 15장 스타일링

--- prac/my-app/src/index.js
+++ prac/my-app/src/index.js
@@ -16,6 +16,7 @@ import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
 import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(() => {
@@ -25,7 +26,7 @@ setInterval(() => {
     // 개발 모드에서 StrictMode를 사용하게 되면, 컴포넌트를 unmount 시켰다가 다시 한번 remount 시키게 됨
     // so, 일단 주석 처리
     // <React.StrictMode>
-      <DarkOrLight />
+      <Blocks />
     // </React.StrictMode>
   );
 }, 1000);
